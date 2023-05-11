e11d82f994d2e092de877f50a2774059b7798c65
MctomSpdo <mctom.spdo@gmail.com>
2023-05-11 11:44:43 +0200

    lecture 2023-05-11

--- src/main/java/at/htl/Main.java
+++ src/main/java/at/htl/Main.java
@@ -13,13 +13,18 @@ public class Main {
         em.getTransaction().begin();
 
         var p1 = new Person("Max", "Muster", LocalDate.of(2000, 1, 1));
-
         p1 = em.merge(p1);
-
         p1.setLastname("Mustermann");
 
-
         em.getTransaction().commit();
+
+        em.getTransaction().begin();
+        var p2 = em.find(Person.class, 1000L);
+        em.remove(p2);
+        em.getTransaction().commit();
+
+        System.out.println(p2.getFirstname());
+
         em.close();
         emf.close();
     }
